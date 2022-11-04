185b7a6bf6d70d714da35f9e9ba39093b601ad03
Roberto Araripe <roberto.araripe@outlook.com>
2022-11-04 11:12:30 -0300

    First commit - project created

---
+++ src/main/java/com/robertoararipe/workshopmongo/WorkshopmongApplication.java
@@ -0,0 +1,13 @@
+package com.robertoararipe.workshopmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopmongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopmongApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/robertoararipe/workshopmongo/WorkshopmongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.robertoararipe.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
