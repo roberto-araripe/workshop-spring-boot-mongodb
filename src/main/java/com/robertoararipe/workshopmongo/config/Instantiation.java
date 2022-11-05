91980ab0b92ba7e3fb31acb9f61eb997397115b8
Roberto Araripe <roberto.araripe@outlook.com>
2022-11-05 08:46:54 -0300

    Referencing user posts

--- src/main/java/com/robertoararipe/workshopmongo/config/Instantiation.java
+++ src/main/java/com/robertoararipe/workshopmongo/config/Instantiation.java
@@ -41,7 +41,10 @@ public class Instantiation implements CommandLineRunner {
 		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem!", "Vou viajar para São Paulo, abraços!", new AuthorDTO(maria));
 		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia!", "Hoje, acordei feliz!", new AuthorDTO(maria));
 				
-		postRepository.saveAll(Arrays.asList(post1, post2)); 
+		postRepository.saveAll(Arrays.asList(post1, post2));
+
+		maria.getPosts().addAll(Arrays.asList(post1, post2));
+		userRepository.save(maria);
 		
 	}
 	
